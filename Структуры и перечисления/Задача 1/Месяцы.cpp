45bb3ca085035a7ba593e4c469dfd8364fd6f3ff
SerFet <mrgrimzif@gmail.com>
2024-02-22 20:08:03 +0300

    Initial commit

---
+++ Структуры и перечисления/Задача 1/Месяцы.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+enum class months 
+{
+	exit = 0,
+	Январь,
+	Февраль,
+	Март,
+	Апрель,
+	Май,
+	Июнь,
+	Июль,
+	Август,
+	Сентябрь,
+	Октябрь,
+	Ноябрь,
+	Декабрь,
+};
+
+string monthToString(months month) {
+	switch (month) {
+	case months::Январь:return "Январь";
+	case months::Февраль:return "Февраль";
+	case months::Март:return "Март";
+	case months::Апрель:return "Апрель";
+	case months::Май:return "Май";
+	case months::Июнь:return "Июнь";
+	case months::Июль:return "Июль";
+	case months::Август:return "Август";
+	case months::Сентябрь:return "Сентябрь";
+	case months::Октябрь:return "Октябрь";
+	case months::Ноябрь:return "Ноябрь";
+	case months::Декабрь:return "Декабрь";
+	}
+}
+
+months monthRefund(int number) {
+	if (number >= 1 && number <= 12) {
+		return months(number);
+	}
+	else {
+		return months::exit;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int number = 0;
+
+	while (true) {
+		cout << "Введите номер месяца: ";
+		cin >> number;
+
+		if (number > 12 || number < 0) {
+			cout << "Неправильный номер!" << endl;
+			continue;
+		}
+		months month = monthRefund(number);
+		if (month == months::exit) {
+			cout << "До свидания!" << endl;
+			break;
+		}
+		else {
+
+			cout << "Название месяца: " << monthToString(month) << endl;
+		}
+	}
+
+	return 0;
+}
---
+++ Структуры и перечисления/Задача 3/Вывод Структуры.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct adressStorage {
+	std::string city;
+	std::string streret;
+	int house;
+	int apartment;
+	int index;
+};
+
+void outputStructure(adressStorage& info) {
+	cout << "Город: " << info.city << endl;
+	cout << "Улица: " << info.streret << endl;
+	cout << "Дом: " << info.house << endl;
+	cout << "Квартира: " << info.apartment << endl;
+	cout << "Индекс: " << info.index << endl;
+	cout << endl;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	adressStorage oneCopy = { "Москва", "Арбат", 3, 125, 125489};
+	adressStorage twoCopy = { "Барнаул", "Пушкина", 1, 6, 759853 };
+	outputStructure(oneCopy);
+	outputStructure(twoCopy);
+}
