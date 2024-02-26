e148e037982873cb3a2b2b966410a0893b47fecc
SerFet <mrgrimzif@gmail.com>
2024-02-26 14:26:51 +0300

    added tasks

---
+++ Классы и объекты/Задача 1/Калькулятор двух чисел.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Calculator
+{
+public:
+	double num1;
+	double num2;
+
+	Calculator() {
+		num1 = 0;
+		num2 = 0;
+	};
+
+	double add() {
+		return num1 + num2;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double subtract_1_2() {
+		return num1 - num2;
+	}
+	double subtract_2_1() {
+		return num2 - num1;
+	}
+	double divide_1_2() {
+		return num1 / num2;
+	}
+	double divide_2_1() {
+		return num2 / num1;
+	}
+
+	bool set_num1(double num1) {
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		else
+			return false;
+	};
+	bool set_num2(double num2) {
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		else
+			return false;
+	};
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	float a, b;
+	Calculator calc;
+
+	while (true)
+	{
+
+		cout << "Введите num1: ";
+		cin >> a;
+		while (calc.set_num1(a) != true)
+		{
+			cout << "Неверный ввод!" << endl;
+			cout << "Введите num1: ";
+			cin >> a;
+		}
+		cout << "Введите num2: ";
+		cin >> b;
+		while (calc.set_num2(b) != true)
+		{
+			cout << "Неверный ввод!" << endl;
+			cout << "Введите num2: ";
+			cin >> b;
+		}
+		
+		cout << "num1 + num2 = " << calc.add() << endl;
+		cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
+		cout << "num1 - num2 = " << calc.subtract_2_1() << endl;
+		cout << "num1 * num2 = " << calc.multiply() << endl;
+		cout << "num1 / num2 = " << calc.divide_1_2() << endl;
+		cout << "num2 / num1 = " << calc.divide_2_1() << endl;
+
+	};
+
+		return 0;
+}
+
---
+++ Классы и объекты/Задача 2/Счётчик.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Counter {
+private:
+	int count;
+
+public:
+	Counter() {
+		count = 0;
+	};
+
+	void plus() {
+		++count;
+	};
+
+	void minus() {
+		--count;
+	};
+
+	int meaning() {
+		return count;
+	}
+
+	void setOne() {
+		count = 1;
+	};
+
+	void setNum(int num) {
+		count = num;
+	};
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int num = 0;
+	string ans;
+	Counter myCounter;
+
+	while (true) {
+		cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+		std::cin >> ans;
+
+
+		if (ans == "yes") {
+			while (true) {
+				cout << "Введите начальное значение счётчика: ";
+				std::cin >> num;
+
+				if (std::cin.fail()) {
+					std::cin.clear();
+					std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout << "Ошибка! Введите целое число." << endl;
+					continue;
+				}
+				else {
+					myCounter.setNum(num);
+					cout << "Начальное значение счётчика = " << myCounter.meaning() << endl;
+					break;
+				}
+			}
+			break;
+		}
+		else if (ans == "no") {
+			myCounter.setOne();
+			cout << "Начальное значение счётчика = " << myCounter.meaning() << endl;
+			break;
+		}
+		else {
+			cout << "Некорректная команда. Попробуйте снова." << endl;
+		}
+	}
+
+
+	char sym = ' ';
+
+	while (sym != 'x') {
+		cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> sym;
+
+		if (sym == '+') {
+			myCounter.plus();
+		}
+		else if (sym == '-') {
+			myCounter.minus();
+		}
+		else if (sym == '=') {
+			cout << myCounter.meaning() << endl;
+		}
+		else if (sym == 'x') {
+			cout << "До свидания!";
+		}
+		else {
+			cout << "Некорректная команда. Попробуйте снова." << endl;
+		}
+	}
+
+	return 0;
+}
