efabcc6b0754c38ab9ed3600a998e9f67ef331f6
MrZef1rka <mrgrimzif@gmail.com>
2024-07-10 20:02:06 +0300

    add tasks

---
+++ Squaring/Squaring.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+T Squaring(T a) {
+    
+   return a * a;
+
+};
+
+template<typename T>
+void Squaring(vector<T>& vec) {
+
+    for (auto& elem : vec)
+    {
+        elem = Squaring(elem);
+    };
+};
+
+int main()
+{
+    std::cout << Squaring(10) << endl;
+    std::cout << Squaring(5) << endl;
+    std::cout << Squaring(8) << endl;
+    std::cout << Squaring(15) << endl;
+
+
+    cout << "-----------" << endl;
+    
+    vector<int> vec = { 10, 5, 8, 15 };
+    Squaring(vec);
+
+    for (const auto& elem : vec) 
+    {
+        cout << elem << endl;
+    };
+
+    return 0;
+}
---
+++ Table/Table.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+template <class T>
+class Array {
+private:
+    T** array;
+    int rows;
+    int cols;
+public:
+    Array(int a, int b) : rows(a), cols(b) {
+    
+        array = new T*[rows];
+
+        for (int i = 0; i < rows; i++) 
+        {
+            array[i] = new T[cols];
+        }
+    
+    };
+
+    ~Array() {
+
+        for (int i = 0; i < rows; i++) {
+        
+            delete[] array[i];
+        }
+    
+        delete[] array;
+        
+    };
+
+    const T* operator[](int index) const {
+    
+        return array[index];
+    }
+
+    T* operator[](int index) {
+
+        return array[index];
+    }
+
+    pair<int, int> Size() const {
+
+        return { rows, cols };
+ 
+    }
+};
+
+
+
+int main()
+{
+    
+    Array<int> arr(2, 3);
+
+    arr[0][0] = 4;
+
+    auto size = arr.Size();
+    
+    cout << arr[0][0] << endl;
+    cout << "Size table arr: " << size.first << "x" << size.second << endl;
+
+    return 0;
+}
+
