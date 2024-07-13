de6a2a5f0ce16ad819e5e29d645a5f02875ef9f7
MrZef1rka <mrgrimzif@gmail.com>
2024-07-13 14:39:31 +0300

    Changes

--- SymbolFrequency/SymbolFrequency.cpp
+++ SymbolFrequency/SymbolFrequency.cpp
@@ -1,32 +1,32 @@
 ﻿#include <iostream>
 #include <map>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-
 static map<char, int> calculateFrequencies(const string& str) {
-
 	map<char, int> frequency_map;
-
 	for (char ch : str) {
-	
+
 		frequency_map[ch]++;
 	}
-	
 	return frequency_map;
-}
+};
 
-int main()
-{
-	
+int main() {
 	string input = "Hello world!!";
-
 	map<char, int> frequency = calculateFrequencies(input);
+	vector<pair<char, int>> frequency_vector(frequency.begin(), frequency.end());
 
-	for (const auto& pair : frequency) {
+	sort(frequency_vector.begin(), frequency_vector.end(),
+		[](const pair<char, int>& a, const pair<char, int>& b) {
+			return a.second > b.second;
+		});
+
+	for (const auto& pair : frequency_vector) {
 
 		cout << pair.first << ": " << pair.second << endl;
 	}
-
-    return 0;
-}
+	return 0;
+};
 
