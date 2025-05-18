9b26f4e52a70ab5d3413285e27f5e1a674ebe31a
Sergey <mrgrimzif@gmail.com>
2025-05-18 17:05:18 +0300

    Code revision

--- PostgreSQLAndC++/main.cpp
+++ PostgreSQLAndC++/main.cpp
@@ -1,356 +1,167 @@
 #include <iostream>
 #include <pqxx/pqxx>
-#include <Windows.h>
+#include <vector>
 #include <string>
-#include <tuple>
+#include <map>
+#include <windows.h>
 
-#pragma execution_character_set("utf-8")
-using namespace std;
-
-
-class Controller {
-public:
-	Controller(pqxx::connection& db_conn) : conn(db_conn) {};
-	~Controller() {};
-
-	void createBD() {
-
-		try {
-			pqxx::work tx(conn);
-			tx.exec("CREATE TABLE IF NOT EXISTS clients ("
-			"id SERIAL PRIMARY KEY, "
-			"name VARCHAR(100), "
-			"surname VARCHAR(100), "
-			"email VARCHAR(100) UNIQUE);");
-			tx.exec("CREATE TABLE IF NOT EXISTS phones ("
-			"id SERIAL PRIMARY KEY, "
-			"client_id INTEGER REFERENCES clients(id) ON DELETE CASCADE, "
-			"phone_number VARCHAR(15));");
-			tx.commit();
-			cout << "Таблицы созданы!" << endl;
-		} catch (const exception& e) {
-			cout << "Ошибка при создании таблиц! " << e.what() << endl;
-		}
-	}
-
-	void checkTable() {
-
-		try
-		{
-			pqxx::work tx(conn);
-
-			auto res = tx.exec("SELECT clients.id, name, surname, email, phone_number FROM clients "
-				"LEFT JOIN phones ON clients.id = phones.client_id;");
-
-			if (res.empty()) {
-				cout << "Таблица пуста." << endl;
-			}
-			else {
-				for (auto row : res) {
-
-					cout << "ID: " << row[0].as<int>() << " | "
-						 << "Имя: " << row[1].as<string>() << " | "
-						 << "Фамилия: " << row[2].as<string>() << " | "
-						 << "Email: " << row[3].as<string>() << " | "
-						 << "Телефон: " << (row[4].is_null() ? "Нет телефона" : row[4].as<string>()) << endl;
-				}
-			}
-		} catch (const exception& e)
-		{
-			cout << "Ошибка при формировании таблицы! " << e.what() << endl;
-		}
-	}
-
-	void addUser() {
-		cout << "Введите имя: ";
-		cin.ignore();
-		getline(cin, name);
-		cout << "Введите фамилию: ";
-		getline(cin, surname);
-		cout << "Введите email: ";
-		getline(cin, email);
-
-		try
-		{
-			pqxx::work tx(conn);
-
-
-			if (name.empty() || surname.empty() || email.empty()) {
-				cout << "Ошибка: все поля должны быть заполнены." << endl;
-				return;
-			}
-
-			string query = "INSERT INTO clients (name, surname, email) VALUES (" 
-					+ tx.quote(name) + ", "
-					+ tx.quote(surname) + ", "
-					+ tx.quote(email) + ");";
-
-			tx.exec(query);
-			tx.commit();
-			cout << "Клиент добавлен!" << endl;
-		}
-		catch (const exception& e)
-		{
-			cout << "Ошибка при добавлении клиента! " << e.what() << endl;
-		}
-	}
-
-	void addNumber() {
-		int client_id;
-		cout << "Введите ID клиента: ";
-
-		while (!(cin >> client_id)) {
-			cout << "Ошибка: введен неверный ID клиента. Пожалуйста, введите число: ";
-			cin.clear(); 
-			cin.ignore(10000, '\n');
-		}
-
-		cin.ignore(); 
-
-		cout << "Введите номер телефона: ";
-		getline(cin, phone_number);
-
-		try {
-			pqxx::work tx(conn);
-			tx.exec("INSERT INTO phones (client_id, phone_number) VALUES ("
-				+ tx.quote(client_id) + ", "
-				+ tx.quote(phone_number) + ");");
-			tx.commit();
-			cout << "Номер телефона добавлен!" << endl;
-		}
-		catch (const std::exception& e) {
-			cout << "Ошибка при добавлении телефона! " << e.what() << endl;
-		}
-	}
-
-
-	void changeUser() {
-		int client_id;
-		cout << "Введите ID клиента, которого хотите изменить: ";
-
-		while (!(cin >> client_id)) {
-			cout << "Ошибка: введен неверный ID клиента. Пожалуйста, введите число: ";
-			cin.clear();
-			cin.ignore(10000, '\n'); 
-		}
-		cin.ignore();
-
-		cout << "Введите новое имя: ";
-		cin >> name;
-		cout << "Введите новую фамилию: ";
-		cin >> surname;
-		cout << "Введите новый email: ";
-		cin >> email;
-
-		try {
-			pqxx::work tx(conn);
-			tx.exec("UPDATE clients SET "
-				"name = " + tx.quote(name) + ", "
-				"surname = " + tx.quote(surname) + ", "
-				"email = " + tx.quote(email) +
-				" WHERE id = " + tx.quote(client_id) + ";");
-			tx.commit();
-			cout << "Данные клиента обновлены!" << endl;
-		}
-		catch (const std::exception& e) {
-			cout << "Ошибка при изменении данных клиента! " << e.what() << endl;
-		}
-	
-	}
-
-	void delNumber() {
-		int client_id;
-		cout << "Введите ID клиента, у которого хотите удалить номер телефона: ";
-
-		while (!(cin >> client_id)) {
-			cout << "Ошибка: введен неверный ID клиента. Пожалуйста, введите число: ";
-			cin.clear(); 
-			cin.ignore(10000, '\n'); 
-		}
-		cin.ignore(); 
-
-		try {
-			pqxx::work tx(conn);
-
-			// Получение всех номеров телефонов клиента
-			pqxx::result res = tx.exec("SELECT id, phone_number FROM phones WHERE client_id = " + tx.quote(client_id) + ";");
-
-			if (res.empty()) {
-				cout << "У клиента с ID " << client_id << " нет номеров телефонов." << endl;
-				return;
-			}
-
-			cout << "Номера телефонов клиента с ID " << client_id << ":" << endl;
-			for (auto row : res) {
-				cout << "ID телефона: " << row[0].as<int>() << " | Номер: " << row[1].as<string>() << endl;
-			}
-
-			int phone_id;
-			cout << "Введите ID телефона, который хотите удалить: ";
-
-			while (!(cin >> phone_id)) {
-				cout << "Ошибка: введен неверный ID телефона. Пожалуйста, введите число: ";
-				cin.clear(); 
-				cin.ignore(10000, '\n'); 
-			}
-			cin.ignore(); 
-
-			pqxx::result del_res = tx.exec("DELETE FROM phones WHERE id = " + tx.quote(phone_id) + " AND client_id = " + tx.quote(client_id) + ";");
-
-			if (del_res.affected_rows() > 0) {
-				cout << "Телефон с ID " << phone_id << " удален!" << endl;
-			}
-			else {
-				cout << "Ошибка: телефон с ID " << phone_id << " не найден у клиента с ID " << client_id << "." << endl;
-			}
-
-			tx.commit();
-		}
-		catch (const std::exception& e) {
-			cout << "Ошибка при удалении телефона! " << e.what() << endl;
-		}
-	}
-
-	void delUser() {
-		int client_id;
-		cout << "Введите ID клиента, которого хотите удалить: ";
-		while (!(cin >> client_id)) {
-			cout << "Ошибка: введен неверный ID клиента. Пожалуйста, введите число: ";
-			cin.clear(); // Сброс состояния ввода
-			cin.ignore(10000, '\n'); // Игнорирование оставшегося ввода
-		}
-		cin.ignore();
-
-		try {
-
-			pqxx::work tx(conn);
-			pqxx::result checkRes = tx.exec("SELECT COUNT(*) FROM clients WHERE id = " + tx.quote(client_id) + ";");
-
-			int count = checkRes[0][0].as<int>();
-			if (count == 0) {
-				cout << "Клиент с ID " << client_id << " не существует." << endl;
-				return;
-			}
-
-			tx.exec("DELETE FROM clients WHERE id = " + tx.quote(client_id) + ";");
-			cout << "Клиент c ID " << client_id << " удален!" << endl;
-			tx.commit();
-		}
-		catch (const std::exception& e) {
-			cout << "Ошибка при удалении клиента! " << e.what() << endl;
-		}
-	}
-
-	void findeUser() {
-		cout << "Введите данные для поиска (имя, фамилия, email или телефон): ";
-		string search_query;
-		cin >> search_query;
-
-		try {
-			pqxx::work tx(conn);
-			pqxx::result r = tx.exec("SELECT clients.id, name, surname, email, phone_number FROM clients "
-				"LEFT JOIN phones ON clients.id = phones.client_id "
-				"WHERE name ILIKE " + tx.quote('%' + search_query + '%') +
-				" OR surname ILIKE " + tx.quote('%' + search_query + '%') +
-				" OR email ILIKE " + tx.quote('%' + search_query + '%') +
-				" OR phone_number ILIKE " + tx.quote('%' + search_query + '%') + ";");
-			if (r.empty()) {
-				cout << "Клиент не найден." << endl;
-			}
-			else {
-				for (auto row : r) {
-					cout << "ID: " << row[0].as<int>() << " | "
-						<< "Имя: " << row[1].as<string>() << " | "
-						<< "Фамилия: " << row[2].as<string>() << " | "
-						<< "Email: " << row[3].as<string>() << " | "
-						<< "Телефон: " << (row[4].is_null() ? "Нет телефона" : row[4].as<string>()) << endl;
-				}
-			}
-		}
-		catch (const std::exception& e) {
-			cout << "Ошибка при поиске клиента! " << e.what() << endl;
-		}
-	}
-private:
-	pqxx::connection& conn;
-	string name;
-	string surname;
-	string email;
-	string phone_number;
+struct Client {
+    int id = -1;
+    std::string name;
+    std::string surname;
+    std::string email;
+    std::vector<std::string> phone_numbers;
 };
 
-int main()
-{
+class ClientManager {
+public:
+    ClientManager(pqxx::connection& conn) : conn(conn) {}
 
-	SetConsoleCP(CP_UTF8);
-	SetConsoleOutputCP(CP_UTF8);
-	//setlocale(LC_ALL, "RU");
+    void createDatabase() {
+        pqxx::work tx(conn);
+        tx.exec(R"(
+            CREATE TABLE IF NOT EXISTS clients (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100),
+                surname VARCHAR(100),
+                email VARCHAR(100) UNIQUE
+            );
+        )");
+   
+        tx.exec(R"(
+            CREATE TABLE IF NOT EXISTS phones (
+                id SERIAL PRIMARY KEY,
+                client_id INTEGER REFERENCES clients(id) ON DELETE CASCADE,
+                phone_number VARCHAR(15)
+            );
+        )");
+        tx.commit();
+    }
+    void prepareStatements()
+    {
+        conn.prepare("insert_client",
+            "INSERT INTO clients (name, surname, email) VALUES ($1, $2, $3) RETURNING id");
 
-	int selection = 0;
+        conn.prepare("insert_phone",
+            "INSERT INTO phones (client_id, phone_number) VALUES ($1, $2)");
 
-	try {
+        conn.prepare("update_client",
+            "UPDATE clients SET name = $1, surname = $2, email = $3 WHERE id = $4");
 
-		pqxx::connection conn(
-			"host=localhost "
-			"port=5432 "
-			"dbname = postgres "
-			"user = TestC++ "
-			"password = password "
-			"client_encoding=UTF8");
+        conn.prepare("delete_phone",
+            "DELETE FROM phones WHERE id = $1");
 
-		cout << "Подключение к базе успешно!" << endl;
+        conn.prepare("delete_client",
+            "DELETE FROM clients WHERE id = $1");
 
-		Controller db(conn);
+        conn.prepare("find_clients",
+            R"(
+            SELECT clients.id, name, surname, email, phone_number
+            FROM clients
+            LEFT JOIN phones ON clients.id = phones.client_id
+            WHERE name ILIKE $1 OR surname ILIKE $1 OR email ILIKE $1 OR phone_number ILIKE $1
+            )"
+        );
+    }
 
-		while (selection != 9) {
-			cout << endl;
-			cout << "Что хотите сделать:" << endl;
-			cout <<
-				"1. Создать таблицу \n"
-				"2. Просмотреть таблицу \n"
-				"3. Добавить нового клиента \n"
-				"4. Добавить телефон для существующего клиента \n"
-				"5. Изменить данные о клиенте \n"
-				"6. Удалить телефон у существующего клиента \n"
-				"7. Удалить существующего клиента \n"
-				"8. Найти клиента по его данным — имени, фамилии, email или телефону \n"
-				"9. Выйти \n";
-			cout << "Ваш выбор: ";
-			cin >> selection;
+    void addClient(const Client& client) {
+        pqxx::work tx(conn);
+        pqxx::result r = tx.exec_prepared("insert_client", client.name, client.surname, client.email);
+        int client_id = r[0][0].as<int>();
+        for (const auto& phone : client.phone_numbers) {
+            tx.exec_prepared("insert_phone", client_id, phone);
+        }
+        tx.commit();
+    }
 
-			switch (selection)
-			{
-			case 1: db.createBD();
-				break;
-			case 2: db.checkTable();
-				break;
-			case 3: db.addUser();
-				break;
-			case 4: db.addNumber();
-				break;
-			case 5: db.changeUser();
-				break;
-			case 6: db.delNumber();
-				break;
-			case 7: db.delUser();
-				break;
-			case 8: db.findeUser();
-				break;
-			case 9:
-				cout << "Выход из программы...";
-				break;
-			default:
-				cout << "Неверный выбор!" << endl;
-				break;
-			}
+    void addPhone(int client_id, const std::string& phone) {
+        pqxx::work tx(conn);
+        tx.exec_prepared("insert_phone", client_id, phone);
+        tx.commit();
+    }
 
-		}
-	}
-	catch (const exception& e)
-	{
-		cout << "Error happened: " << e.what() << endl;
-	}
+    void updateClient(int client_id, const Client& client) {
+        pqxx::work tx(conn);
+        tx.exec_prepared("update_client", client.name, client.surname, client.email, client_id);
+        tx.commit();
+    }
 
+    void deletePhone(int phone_id) {
+        pqxx::work tx(conn);
+        tx.exec_prepared("delete_phone", phone_id);
+        tx.commit();
+    }
 
-	return 0;
+    void deleteClient(int client_id) {
+        pqxx::work tx(conn);
+        tx.exec_prepared("delete_client", client_id);
+        tx.commit();
+    }
+
+    std::vector<Client> findClients(const std::string& query) {
+        pqxx::work tx(conn);
+        std::vector<Client> results;
+
+        std::string pattern = "%" + query + "%";
+        pqxx::result r = tx.exec_prepared("find_clients", pattern);
+
+        std::map<int, Client> clients_map;
+        for (auto row : r) {
+            int id = row[0].as<int>();
+            Client& client = clients_map[id];
+            client.id = id;
+            client.name = row[1].as<std::string>();
+            client.surname = row[2].as<std::string>();
+            client.email = row[3].as<std::string>();
+            if (!row[4].is_null()) {
+                client.phone_numbers.push_back(row[4].as<std::string>());
+            }
+        }
+
+        for (auto& [_, client] : clients_map) {
+            results.push_back(client);
+        }
+
+        return results;
+    }
+
+private:
+    pqxx::connection& conn;
+};
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    try {
+        pqxx::connection conn(
+            "host=localhost port=5432 dbname=TestCpp user=postgres password=123456"
+        );
+
+        ClientManager manager(conn);
+        manager.createDatabase();
+        manager.prepareStatements();
+
+        Client newClient;
+        newClient.name = "Ivan";
+        newClient.surname = "Ivanov";
+        newClient.email = "ivan@example.com";
+        newClient.phone_numbers = {"123456", "987654"};
+        manager.addClient(newClient);
+
+        auto found = manager.findClients("ivan");
+        for (const auto& c : found) {
+            std::cout << "ID: " << c.id << " | " << c.name << " " << c.surname
+                      << " | " << c.email << "\n";
+            for (const auto& phone : c.phone_numbers) {
+                std::cout << "   Phone: " << phone << "\n";
+            }
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    std::cin.get();
+
+    return 0;
 }
